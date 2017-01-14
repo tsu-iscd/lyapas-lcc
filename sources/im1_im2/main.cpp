9a01ba1aa2015337a185426cc1061e86b9d5a79d
Safonov Vadim <v.o.safonov.94@gmail.com>
2017-01-14 19:53:23 +0700

    [*] подготовка кода парсинга json для переноса в отдельный модуль

--- sources/im1_im2/main.cpp
+++ sources/im1_im2/main.cpp
@@ -6,32 +6,25 @@
 
 using namespace std;
 
-//***TEST PARSE: Рекрсивный парсер дерева. которое затем выводится в консоль
-// В филнальной версии парсер будет реализован в отдельном классе и для каждого узла дерева
-// будет создавать нужную реализацию ICmd.
-// ***/
 Json::Value tryParseJson(istream &stream)
 {
     Json::Value json;
     Json::Reader reader;
 
-    bool parsingSuccessful = reader.parse(stream, json);
-    if(!parsingSuccessful)
+    if(reader.parse(stream, json))
     {
-        cout << "Json Parse Error: " << endl;
-        cout << reader.getFormattedErrorMessages() << endl;
+        return json;
     }
 
-    return json;
+    throw std::runtime_error("Json Parse Error: " + reader.getFormattedErrorMessages());
 }
-//*** END TEST PARSE ***/
 
 int main(int argc, char* argv[])
 {
     Json::Value jsonTree;
     if(argc < 2)
     {
-        std::cin >> jsonTree;
+        jsonTree = tryParseJson(std::cin);
     }
     else
     {
@@ -39,12 +32,7 @@ int main(int argc, char* argv[])
 
         ifstream jsonFile;
         jsonFile.open(pathToFile, std::ifstream::binary);
-        //TODO:
-        //This functions doesn't look realy good
-        //it tries to parse json, but if it can't parse json
-        //it will print message and nothing more
         jsonTree = tryParseJson(jsonFile);
-        //Maybe there should be a reaction(for failed parsing)
     }
 
     CmdFactory factory;
