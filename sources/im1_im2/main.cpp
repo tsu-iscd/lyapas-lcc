bbf3230314e1f0044cef9042b5586eeb511a9177
Safonov Vadim <v.o.safonov.94@gmail.com>
2017-01-14 22:13:41 +0700

    [+] шаблонная функция для ввода программы, парсинга, трансляции и вывода программы

--- sources/im1_im2/main.cpp
+++ sources/im1_im2/main.cpp
@@ -1,23 +1,22 @@
-#include <iostream>
 #include <json/json.h>
-#include <shared_utils/json_helper.h>
+#include <shared_utils/translation_helper.h>
 #include "TreeParser/IParser.h"
 #include "TreeParser/TreeParser.h"
 
+class Odnoyaz {
+public:
+    Odnoyaz() {}
+
+    void translate(Json::Value &json)
+    {
+        CmdFactory factory;
+        TreeParser treeParser(factory);
+        json = treeParser.parseTree(json)->toJson();
+    }
+};
+
 int main(int argc, char* argv[])
 {
-    Json::Value jsonTree;
-    if(argc < 2) {
-        sutils::parseJsonFromStream(std::cin, jsonTree);
-    } else {
-        sutils::parseJsonFromFile(argv[1], jsonTree);
-    }
-
-    CmdFactory factory;
-    TreeParser treeParser(factory);
-
-    Json::Value jsonResult = treeParser.parseTree(jsonTree)->toJson();
-    sutils::writeJsonToStream(std::cout, jsonResult);
-
+    sutils::translateSimple<Odnoyaz>(argc, argv);
     return 0;
 }
