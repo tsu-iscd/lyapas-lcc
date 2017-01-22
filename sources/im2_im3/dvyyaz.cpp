ad90fa6a1aed8dc2b7c6e571724ad1e691230fcc
Safonov Vadim <v.o.safonov.94@gmail.com>
2017-01-22 12:16:03 +0700

    [+] класс Complexoyaz; маленькие правки в файлах правил трансляции

--- sources/im2_im3/dvyyaz.cpp
+++ sources/im2_im3/dvyyaz.cpp
@@ -1,27 +1,31 @@
 #include "dvyyaz.h"
 
-bool dyaz::Dvyyaz::valid(const JSON &cmds, std::string &error)
+namespace dyaz {
+
+bool Dvyyaz::valid(const JSON &cmds, std::string &error)
 {
     return true;
 }
 
-void dyaz::Dvyyaz::preprocess(JSON &cmds)
+void Dvyyaz::preprocess(JSON &cmds)
 {
 }
 
-void dyaz::Dvyyaz::postprocess(JSON &cmds)
+void Dvyyaz::postprocess(JSON &cmds)
 {
 }
 
-const trm::Replacers &dyaz::Dvyyaz::getReplacers(const JSON &cmds)
+const trm::Replacers &Dvyyaz::getReplacers(const JSON &cmds)
 {
     static trm::Replacers replacers;
     return replacers;
 }
 
-std::string dyaz::Dvyyaz::getRules()
+std::string Dvyyaz::getRules()
 {
     return std::string(
 #include "rules.txt"
     );
+}
+
 }
---
+++ sources/im4_im5/complexoyaz.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <translation_module/translation_module.h>
+
+namespace cyaz {
+
+class Complexoyaz : public trm::TranslationModule {
+private:
+    virtual bool valid(const JSON &cmds, std::string &error) override;
+    virtual void preprocess(JSON &cmds) override;
+    virtual void postprocess(JSON &cmds) override;
+
+    virtual const trm::Replacers &getReplacers(const JSON &cmds) override;
+    virtual std::string getRules() override;
+};
+
+}
+
+
