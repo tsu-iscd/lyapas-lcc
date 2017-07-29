cfad87fdb0f9cd927f0a2402ab275043e50b59f4
Yulia Khalniyazova <juliajarskilli@gmail.com>
2017-07-29 16:41:48 +0700

    Added reading from stdin in lyapas_to_json.py

--- lyapas_to_json.py
+++ lyapas_to_json.py
@@ -4,15 +4,27 @@ import argparse
 import codecs
 
 def main(args):
-    with open(args.in_lyapas) as in_f, open(args.out_filename, 'w') as out_f:
-        data = in_f.read()
+    if args.in_lyapas is None:
+        data = ''
+        while True:
+            try:
+                data += raw_input() + '\n'
+            except EOFError:
+                break
         data = json.dumps(data, ensure_ascii=False, encoding='utf-8')
-        json_data = '{"file": "' + in_f.name + '",' + ' "source": ' + data +'}'
+        json_data = '{"file": stdin, "source": ' + data +'}'
+    else:
+        with open(args.in_lyapas) as in_f: 
+            data = in_f.read()
+            data = json.dumps(data, ensure_ascii=False, encoding='utf-8')
+            json_data = '{"file": "' + in_f.name + '",' + ' "source": ' + data +'}' 
+    
+    with open(args.out_filename, 'w') as out_f:     
         out_f.write(json_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Getting json from lyapas sourses')
-    parser.add_argument("in_lyapas", help='Path in filesystem for input lyapas-file')
-    parser.add_argument('-out_filename', default='ex.json.lyapas', help='Name of output file')
+    parser.add_argument("in_lyapas", help='Path in filesystem for input lyapas-file', default=None, nargs='?')
+    parser.add_argument('-out_filename', default='ex.json.lyapas', help='Name of output file', nargs='?')
     args = parser.parse_args()
     main(args)
