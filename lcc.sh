def513274c79f788ba7d5967ad58b5aa3950062a
Yulia Khalniyazova <juliajarskilli@gmail.com>
2017-10-18 13:15:22 +0700

    pipeline updated with lyapas_to_json.py; duplication in using ims removed; relative paths converted to absolute paths

--- lcc.sh
+++ lcc.sh
@@ -2,7 +2,9 @@
 
 dir=false
 allim=false
-cmd="./lcc.sh "
+program_to_run=("lyapas_to_json.py" "lyapas_im0" "im0_im1" "im1_im2" "im2_im3")
+path_to_im="`readlink -e ${0/lcc.sh/}`/build/"
+path_to_json_formatter="`readlink -e ${0/lcc.sh/}`/"
 
 if (($# < 1)) ; then
   echo "The argument must be given"
@@ -16,7 +18,7 @@ for arg in "$@"; do
       ;;
     --allim) 
       allim=true 
-      cmd+="--allim " 
+      cmd+=" --allim "
       ;;
     *)
       if $dir; then
@@ -26,22 +28,24 @@ for arg in "$@"; do
         fi
         dir=false
         for lyapas_file in $(find $arg -name "*.lyapas"); do
-          $cmd $lyapas_file                   
+          $0 $lyapas_file
         done
       elif [[ $arg == *.lyapas ]]; then 
         name=${arg/.lyapas/}
         if $allim; then
-          cat $arg | ./build/lyapas_im0 > "$name.im0"
-          cat "$name.im0" | ./build/im0_im1 > "$name.im1"
-          cat "$name.im1" | ./build/im1_im2 > "$name.im2"
-          cat "$name.im2" | ./build/im2_im3 > "$name.im3"
+          cat $arg | "${path_to_json_formatter}${program_to_run[0]}"> "$name.json"
+          cat "$name.json" | "${path_to_im}${program_to_run[1]}" > "$name.im0"
+          cat "$name.im0" | "${path_to_im}${program_to_run[2]}" > "$name.im1"
+          cat "$name.im1" | "${path_to_im}${program_to_run[3]}" > "$name.im2"
+          cat "$name.im2" | "${path_to_im}${program_to_run[4]}" > "$name.im3"
         else
           name+=".im3"
           cat $arg \
-            | ./build/lyapas_im0 \
-            | ./build/im0_im1 \
-            | ./build/im1_im2 \
-            | ./build/im2_im3 > $name
+            | "${path_to_json_formatter}${program_to_run[0]}" \
+            | "${path_to_im}${program_to_run[1]}" \
+            | "${path_to_im}${program_to_run[2]}" \
+            | "${path_to_im}${program_to_run[3]}" \
+            | "${path_to_im}${program_to_run[4]}" > $name
         fi
         TXTCOLOR_DEFAULT="\033[0;m"
         TXTCOLOR_GREEN="\033[0;32m"
