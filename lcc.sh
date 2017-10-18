f60681649d3411017ce75018e1877b4a45647294
Yulia Khalniyazova <juliajarskilli@gmail.com>
2017-10-18 13:33:39 +0700

    translation function extracted

--- lcc.sh
+++ lcc.sh
@@ -6,6 +6,29 @@ program_to_run=("lyapas_to_json.py" "lyapas_im0" "im0_im1" "im1_im2" "im2_im3")
 path_to_im="`readlink -e ${0/lcc.sh/}`/build/"
 path_to_json_formatter="`readlink -e ${0/lcc.sh/}`/"
 
+function translate()
+{
+  name=${1/.lyapas/}
+  if $allim; then
+    cat $1 | "${path_to_json_formatter}${program_to_run[0]}"> "$name.json"
+    cat "$name.json" | "${path_to_im}${program_to_run[1]}" > "$name.im0"
+    cat "$name.im0" | "${path_to_im}${program_to_run[2]}" > "$name.im1"
+    cat "$name.im1" | "${path_to_im}${program_to_run[3]}" > "$name.im2"
+    cat "$name.im2" | "${path_to_im}${program_to_run[4]}" > "$name.im3"
+  else
+    name+=".im3"
+    cat $1 \
+      | "${path_to_json_formatter}${program_to_run[0]}" \
+      | "${path_to_im}${program_to_run[1]}" \
+      | "${path_to_im}${program_to_run[2]}" \
+      | "${path_to_im}${program_to_run[3]}" \
+      | "${path_to_im}${program_to_run[4]}" > $name
+  fi
+  TXTCOLOR_DEFAULT="\033[0;m"
+  TXTCOLOR_GREEN="\033[0;32m"
+    echo -e $TXTCOLOR_GREEN"Processed $1"$TXTCOLOR_DEFAULT
+}
+
 if (($# < 1)) ; then
   echo "The argument must be given"
   exit 1
@@ -28,28 +51,10 @@ for arg in "$@"; do
         fi
         dir=false
         for lyapas_file in $(find $arg -name "*.lyapas"); do
-          $0 $lyapas_file
+          translate $lyapas_file
         done
       elif [[ $arg == *.lyapas ]]; then 
-        name=${arg/.lyapas/}
-        if $allim; then
-          cat $arg | "${path_to_json_formatter}${program_to_run[0]}"> "$name.json"
-          cat "$name.json" | "${path_to_im}${program_to_run[1]}" > "$name.im0"
-          cat "$name.im0" | "${path_to_im}${program_to_run[2]}" > "$name.im1"
-          cat "$name.im1" | "${path_to_im}${program_to_run[3]}" > "$name.im2"
-          cat "$name.im2" | "${path_to_im}${program_to_run[4]}" > "$name.im3"
-        else
-          name+=".im3"
-          cat $arg \
-            | "${path_to_json_formatter}${program_to_run[0]}" \
-            | "${path_to_im}${program_to_run[1]}" \
-            | "${path_to_im}${program_to_run[2]}" \
-            | "${path_to_im}${program_to_run[3]}" \
-            | "${path_to_im}${program_to_run[4]}" > $name
-        fi
-        TXTCOLOR_DEFAULT="\033[0;m"
-        TXTCOLOR_GREEN="\033[0;32m"
-        echo -e $TXTCOLOR_GREEN"Processed $arg"$TXTCOLOR_DEFAULT
+        translate $arg
       else
         echo "Incorrect argument value $arg"
         exit 1
