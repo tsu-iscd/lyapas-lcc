6614dc1ca37ca99e9f5b03a4de845573b707c379
Yulia Khalniyazova <juliajarskilli@gmail.com>
2017-10-19 18:40:26 +0700

    paths corrected

--- lcc.sh
+++ lcc.sh
@@ -2,27 +2,29 @@
 
 dir=false
 allim=false
-program_to_run=("lyapas_to_json.py" "lyapas_im0" "im0_im1" "im1_im2" "im2_im3")
-path_to_im="`readlink -e ${0/lcc.sh/}`/build/"
-path_to_json_formatter="`readlink -e ${0/lcc.sh/}`/"
+path_to_lyapas_dir="`readlink -e ${0/lcc.sh/}`"
+program_to_run=($path_to_lyapas_dir"/lyapas_to_json.py" 
+                $path_to_lyapas_dir"/build/lyapas_im0" 
+                $path_to_lyapas_dir"/build/im0_im1" 
+                $path_to_lyapas_dir"/build/im1_im2" 
+                $path_to_lyapas_dir"/build/im2_im3")
+suffix=(".lyapas" ".json" ".im0" ".im1" ".im2" ".im3")
 
 function translate()
 {
   name=${1/.lyapas/}
   if $allim; then
-    cat $1 | "${path_to_json_formatter}${program_to_run[0]}"> "$name.json"
-    cat "$name.json" | "${path_to_im}${program_to_run[1]}" > "$name.im0"
-    cat "$name.im0" | "${path_to_im}${program_to_run[2]}" > "$name.im1"
-    cat "$name.im1" | "${path_to_im}${program_to_run[3]}" > "$name.im2"
-    cat "$name.im2" | "${path_to_im}${program_to_run[4]}" > "$name.im3"
+    for i in ${!program_to_run[@]}; do
+        cat "$name${suffix[$i]}" | ${program_to_run[$i]} > "$name${suffix[$i + 1]}"
+    done
   else
     name+=".im3"
     cat $1 \
-      | "${path_to_json_formatter}${program_to_run[0]}" \
-      | "${path_to_im}${program_to_run[1]}" \
-      | "${path_to_im}${program_to_run[2]}" \
-      | "${path_to_im}${program_to_run[3]}" \
-      | "${path_to_im}${program_to_run[4]}" > $name
+      | "${program_to_run[0]}" \
+      | "${program_to_run[1]}" \
+      | "${program_to_run[2]}" \
+      | "${program_to_run[3]}" \
+      | "${program_to_run[4]}" > $name
   fi
   TXTCOLOR_DEFAULT="\033[0;m"
   TXTCOLOR_GREEN="\033[0;32m"
