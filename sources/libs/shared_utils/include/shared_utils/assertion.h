5daf592e7cf26a28e3a90db936463c57356392f4
vosafonov <v.o.safonov.94@gmail.com>
2018-02-03 23:29:35 +0700

    [+] расширил assertion макросы

--- sources/libs/shared_utils/include/shared_utils/assertion.h
+++ sources/libs/shared_utils/include/shared_utils/assertion.h
@@ -1,5 +1,35 @@
 #pragma once
 
-#include <cassert>
+#include <iostream>
+#include <stdexcept>
 
-#define LCC_ASSERT(cond) assert(cond)
+#define LCC_ASSERT(cond)                      \
+    if (!(cond)) {                            \
+        lcc::fail(__FILE__, __LINE__, #cond); \
+    }
+
+#define LCC_FAIL(msg) lcc::fail(__FILE__, __LINE__, msg)
+
+#define LCC_ENSURE(cond, msg)                                                               \
+    if (!(cond)) {                                                                          \
+        throw std::runtime_error{lcc::makeFailMessage(__FILE__, __LINE__, #cond ", " msg)}; \
+    }
+
+namespace lcc {
+
+inline std::string makeFailMessage(const char *file, int line, const char *msg)
+{
+    // "file(line): invariant violation 'msg'"
+    std::string res = file;
+    res += '(' + std::to_string(line) + "): ";
+    res += "invariant violation '" + std::string{msg} + "'";
+    return res;
+}
+
+[[noreturn]] inline void fail(const char *file, int line, const char *msg)
+{
+    std::cout << makeFailMessage(file, line, msg) << std::endl;
+    abort();
+}
+
+}  // namespace lcc
