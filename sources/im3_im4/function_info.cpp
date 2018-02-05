40cb359a434693f94ab2064f635820bad3769490
MashaSamoylova <mashanedyak@gmail.com>
2018-02-06 01:30:00 +0700

    исправили инициализацию

--- sources/im3_im4/function_info.cpp
+++ sources/im3_im4/function_info.cpp
@@ -8,11 +8,11 @@ FunctionInfo::FunctionInfo(const JSON &cmd)
     const JSON &args = cmd["args"];
     LCC_ASSERT(args.size() != 0);
 
-    name = args.operator[](0).asString();
+    name = args[0].asString();
 
     auto slash = std::find(args.begin(), args.end(), "/");
     LCC_ASSERT(slash != args.end());
 
-    input = std::vector<JSON>(++args.begin(), slash);
-    output = std::vector<JSON>(++slash, args.end());
+    input = std::vector<JSON>{++args.begin(), slash};
+    output = std::vector<JSON>{++slash, args.end()};
 }
