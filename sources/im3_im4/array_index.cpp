c7959d653cf03bdecf294662701f94cdaaff4246
MashaSamoylova <mashanedyak@gmail.com>
2018-02-18 00:47:54 +0700

    добавила проверку в конструктор ArrayIndex

--- sources/im3_im4/array_index.cpp
+++ sources/im3_im4/array_index.cpp
@@ -1,7 +1,9 @@
 #include "array_index.h"
+#include <shared_utils/assertion.h>
 
 ArrayIndex::ArrayIndex(const std::string &var)
     : arrayName{var.begin(), var.begin() + var.find("[")}
     , index{var.begin() + var.find("[") + 1, var.end() - 1}
 {
+    LCC_ASSERT(var.back() == ']');
 }
