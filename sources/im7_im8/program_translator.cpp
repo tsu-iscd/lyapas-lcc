02ee8502e4103fcaf24b15b5f805d8100b7e8faa
vosafonov <v.o.safonov.94@gmail.com>
2018-05-05 19:43:47 +0700

    [+] map с отображением команд -> функция-обработчик для этой команды

--- sources/im7_im8/program_translator.cpp
+++ sources/im7_im8/program_translator.cpp
@@ -4,9 +4,10 @@
 
 namespace ayaz {
 
-void ProgramTranslator::process(Program &program)
+void ProgramTranslator::process(Program &program_)
 {
-    for (auto current = std::begin(program); current != std::end(program); ++current) {
+    program = &program_;
+    for (current = std::begin(program_); current != std::end(program_); ++current) {
         JSON &cmd = *current;
 
         if (cmd.isMember("type") && cmd["type"] == "label") {
@@ -25,65 +26,74 @@ void ProgramTranslator::process(Program &program)
             continue;
         }
 
-        std::string cmdName = cmd["cmd"].asString();
-        if (cmdName == "stack_alloc") {
-            LCC_ASSERT(cmd["args"].size() == 1);
-            auto bytes = cmd["args"][0].asUInt64();
-
-            cmd = makeCmd("sub", {"rsp", bytes * 8});
-            continue;
-        }
-
-        if (cmdName == "stack_free") {
-            LCC_ASSERT(cmd["args"].size() == 1);
-            auto bytes = cmd["args"][0].asUInt64();
-
-            cmd = makeCmd("add", {"rsp", bytes * 8});
-            continue;
-        }
-
-        if (cmdName == "definition_string") {
-            LCC_ASSERT(cmd["args"].size() == 2);
-            const std::string &str = cmd["args"][0].asString();
-            const JSON &var = cmd["args"][1];
-
-            std::string alias = "__STR" + std::to_string(strings.size());
-            strings.emplace(str, alias);
-            alias = strings[str];
-
-            cmd = makeCmd("mov", {var, alias});
-            continue;
-        }
-
-        if (cmdName == "write_string") {
-            LCC_ASSERT(cmd["args"].size() == 2);
-            std::string var = cmd["args"][0].asString();
-            JSON len = cmd["args"][1];
-
-            Program inner{makeCmd("mov", {"rax", 1}),
-                          makeCmd("mov", {"rdi", 1}),
-                          makeCmd("mov", {"rsi", var}),
-                          makeCmd("mov", {"rdx", len}),
-                          makeCmd("syscall")};
-
-            current = program.erase(current);
-            program.insert(current, std::begin(inner), std::end(inner));
-            --current;
-            continue;
+        auto found = handlers.find(cmd["cmd"].asString());
+        if (found != std::end(handlers)) {
+            Handler handler = found->second;
+            (this->*handler)();
         }
     }
 
     //
     // FIXME: добавлять только для main функции
     //
-    program.push_back(makeCmd("mov", {"rax", 60}));
-    program.push_back(makeCmd("mov", {"rdi", 0}));
-    program.push_back(makeCmd("syscall"));
+    program_.push_back(makeCmd("mov", {"rax", 60}));
+    program_.push_back(makeCmd("mov", {"rdi", 0}));
+    program_.push_back(makeCmd("syscall"));
 }
 
 auto ProgramTranslator::getStringStorage() -> const StringStorage &
 {
     return strings;
+}
+
+void ProgramTranslator::handleStackAlloc()
+{
+    JSON &cmd = *current;
+    LCC_ASSERT(cmd["args"].size() == 1);
+    auto bytes = cmd["args"][0].asUInt64();
+
+    cmd = makeCmd("sub", {"rsp", bytes * 8});
+}
+
+void ProgramTranslator::handleStackFree()
+{
+    JSON &cmd = *current;
+    LCC_ASSERT(cmd["args"].size() == 1);
+    auto bytes = cmd["args"][0].asUInt64();
+
+    cmd = makeCmd("add", {"rsp", bytes * 8});
+}
+
+void ProgramTranslator::handleDefinitionString()
+{
+    JSON &cmd = *current;
+    LCC_ASSERT(cmd["args"].size() == 2);
+    const std::string &str = cmd["args"][0].asString();
+    const JSON &var = cmd["args"][1];
+
+    std::string alias = "__STR" + std::to_string(strings.size());
+    strings.emplace(str, alias);
+    alias = strings[str];
+
+    cmd = makeCmd("mov", {var, alias});
+}
+
+void ProgramTranslator::handleWriteString()
+{
+    JSON &cmd = *current;
+    LCC_ASSERT(cmd["args"].size() == 2);
+    std::string var = cmd["args"][0].asString();
+    JSON len = cmd["args"][1];
+
+    Program inner{makeCmd("mov", {"rax", 1}),
+                  makeCmd("mov", {"rdi", 1}),
+                  makeCmd("mov", {"rsi", var}),
+                  makeCmd("mov", {"rdx", len}),
+                  makeCmd("syscall")};
+
+    current = program->erase(current);
+    program->insert(current, std::begin(inner), std::end(inner));
+    --current;
 }
 
 }
--- sources/im7_im8/program_translator.h
+++ sources/im7_im8/program_translator.h
@@ -14,7 +14,31 @@ public:
     const StringStorage &getStringStorage();
 
 private:
+    void handleStackAlloc();
+    void handleStackFree();
+    void handleDefinitionString();
+    void handleWriteString();
+
+    using Handler = void (ProgramTranslator::*)();
+    using Handlers = std::map<std::string, Handler>;
+
+    Handlers makeHandlers();
+    Handlers handlers = makeHandlers();
+
     StringStorage strings;
+
+    Program *program = nullptr;
+    Program::iterator current;
 };
+
+inline auto ProgramTranslator::makeHandlers() -> Handlers
+{
+    return {
+        {"stack_alloc", &ProgramTranslator::handleStackAlloc},
+        {"stack_free", &ProgramTranslator::handleStackFree},
+        {"definition_string", &ProgramTranslator::handleDefinitionString},
+        {"write_string", &ProgramTranslator::handleWriteString},
+    };
+}
 
 }
