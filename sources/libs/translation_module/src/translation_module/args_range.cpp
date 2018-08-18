52600b9ab4248bc2344107cdbce344089f0bb547
vosafonov <v.o.safonov.94@gmail.com>
2018-08-18 10:38:06 +0700

    [-] фикс ошибки компиляции на GCC 7

--- sources/libs/translation_module/src/translation_module/args_range.cpp
+++ sources/libs/translation_module/src/translation_module/args_range.cpp
@@ -27,7 +27,7 @@ ArgsRange::ArgsRange(const Filters &filters, JSON &cmd)
         //
         // пропускаем фильтрацию
         //
-        std::transform(std::begin(args), std::end(args), std::back_inserter(filteredArgs), std::addressof<JSON>);
+        std::transform(std::begin(args), std::end(args), std::back_inserter(filteredArgs), [](JSON &json) { return &json; });
         return;
     }
     const ArgsFilter &filter = found->second;
